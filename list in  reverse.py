7bff37e8dfdce82068decf1d8b092f92741fb433
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-12-03 13:28:15 +0530

    Create list in  reverse.py

---
+++ list in  reverse.py
@@ -0,0 +1,8 @@
+#How to reverse in list 
+list=['raju',11,'rajput',11.0]
+list.reverse()
+print(list) # output [11.0, 'rajput', 11, 'raju']
+
+list=['a', 'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u' ,'v' ,'w' ,'x' ,'y' ,'z']
+list.reverse()
+print(list) #output ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
