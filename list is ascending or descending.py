7d025f0c0fbba04f655d21b2a33443369420d4e0
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-12-03 13:21:16 +0530

    Create list is ascending or descending.py

---
+++ list is ascending or descending.py
@@ -0,0 +1,11 @@
+#sorts in ascending order print 'None'
+list=[1,2,3,4,5]
+print(list.sort()) #output is 'None'
+print(list) # output [1,2,3,4,5]
+
+
+
+# sorts in descending order print
+list=[1,2,3,4,5,6]
+list.sort(reverse=True)
+print(list) #output [6,5,4,3,2,1]
