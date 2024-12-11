2029f7d14fe9345575f5f5f16b588cc133736d75
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-12-11 18:44:43 +0530

    Create Speak Easy Simple TTS with Python.py
    Create Speak Easy Simple TTS with Python
    #speak easy
    #python
    #ai
    #pyttsx3
    #import

---
+++ SpeakEasySimple TTS with Python.py
@@ -0,0 +1,5 @@
+                                                                                         #"SpeakEasy: Simple TTS with Python" 
+import pyttsx3
+gf=pyttsx3.init()
+gf.say('welcome back my channel my channel name is coding club')
+gf.runAndWait()
